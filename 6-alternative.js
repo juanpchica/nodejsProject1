adb9b77faf6ae12acc94b8b3206fd81fb992aef1
Juan Pablo Chica <juanpchica@hotmail.com>
2021-04-21 17:20:09 -0500

    New exports method for module added

---
+++ 6-alternative.js
@@ -0,0 +1,7 @@
+const items = ['item1','item2']
+module.exports.person = {
+    name: 'bob',
+}
+
+console.log(module)
+
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const names = require('./4-names')
 const sayHi = require('./utils')
 console.log(module);
 
+require('./7-mind-greneda')
+
 sayHi(names.juan);
 sayHi(names.pablo);
 
